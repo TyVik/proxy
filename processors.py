6b092bf1f64e7f7efc720a094ab0a076a5cd816a
Viktor Tyshchenko <viktor.tyshchenko@3d4medical.com>
2017-06-04 18:43:06 +0300

    Add type hinting for Python 3.6.

--- processors.py
+++ processors.py
@@ -1,11 +1,12 @@
 import re
+from typing import Iterable
 
-from bs4 import Comment
+from bs4 import Comment, BeautifulSoup
 
 SYMBOL_REGEXP = re.compile(r"\b(\w{6})\b", re.IGNORECASE)
 
 
-def modify_links(soup, site, host_url):
+def modify_links(soup: BeautifulSoup, site: str, host_url: str) -> BeautifulSoup:
     for a in soup.findAll('a'):
         link = a.get('href', None)
         if link is not None:
@@ -13,11 +14,11 @@ def modify_links(soup, site, host_url):
     return soup
 
 
-def set_tm(text, symbol):
+def set_tm(text: str, symbol: str) -> str:
     return SYMBOL_REGEXP.sub("\\1" + symbol, text)
 
 
-def modify_text(soup, symbol, skip_tags):
+def modify_text(soup: BeautifulSoup, symbol: str, skip_tags: Iterable[str]) -> BeautifulSoup:
     for text in soup.findAll(text=True):
         if len(text) > 5 and not isinstance(text, Comment):
             if text.parent is not None and text.parent.name in skip_tags:
