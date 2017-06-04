a560a41a3e614dfe666baa82e440d4a2f8a75a99
Viktor Tyshchenko <viktor.tyshchenko@3d4medical.com>
2017-06-04 11:20:12 +0300

    Add logging just in case.

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from flask import Flask, Response, request
+import logging
 import requests
 
 PROXY_SITE = 'https://habrahabr.ru'
 
 app = Flask(__name__.split('.')[0])
+logging.basicConfig(level=logging.DEBUG)
+LOG = logging.getLogger("main.py")
 
 
 @app.route('/<path:url>')
 def proxy(url):
-    r = requests.get('{site}{url}'.format(site=PROXY_SITE, url=request.full_path))
+    url = '{site}{url}'.format(site=PROXY_SITE, url=request.full_path)
+    r = requests.get(url)
+    LOG.debug("Got %s response from %s", r.status_code, url)
     content = r.content
     return Response(content)
 
