75f3b6d01bb7f730450c122850b21860c849a371
Viktor Tyshchenko <viktor.tyshchenko@3d4medical.com>
2017-06-04 12:00:45 +0300

    Replace links by local.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from flask import Flask, Response, request
 import logging
 import requests
@@ -11,10 +12,23 @@ LOG = logging.getLogger("main.py")
 
 @app.route('/<path:url>')
 def proxy(url):
+    def modify_links(soup):
+        for a in soup.findAll('a'):
+            link = a.get('href', None)
+            if link is None:
+                LOG.debug('Broken link on page %s', url)
+                continue
+            a['href'] = link.replace("{}/".format(PROXY_SITE), request.host_url)
+        return soup
+
     url = '{site}{url}'.format(site=PROXY_SITE, url=request.full_path)
     r = requests.get(url)
     LOG.debug("Got %s response from %s", r.status_code, url)
     content = r.content
+    if 'text/html' in r.headers['Content-Type']:
+        # exclude static from parsing
+        soup = BeautifulSoup(content.decode(), "html.parser")  # OPTIMIZE: use lxml instead html.parser
+        content = str(modify_links(soup))
     return Response(content)
 
 
