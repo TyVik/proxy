bdd7a9c55450f16f5d660c2eeed6324e80fb096b
Viktor Tyshchenko <viktor.tyshchenko@3d4medical.com>
2017-06-04 18:58:43 +0300

    Add test for modify_links.

--- tests.py
+++ tests.py
@@ -1,9 +1,17 @@
 import unittest
 
-from processors import set_tm
+from bs4 import BeautifulSoup
+
+from processors import set_tm, modify_links
 
 
 class ProcessorTestCase(unittest.TestCase):
+    def test_modify_links(self):
+        example = '<div><a></a><a href="http://old.com/all/">Text</a></div>'
+        soup = BeautifulSoup(example, "html.parser")
+        result = modify_links(soup, 'http://old.com', 'https://new.com')
+        self.assertEqual(str(result), '<div><a></a><a href="https://new.com/all/">Text</a></div>')
+
     def test_set_tm(self):
         example = 'Client web client.Client wrong connection client.'
         result = set_tm(example, '^')
