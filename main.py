3f0a3ac9d8a6d5bbc5b2037c84b890f90a3a3210
Viktor Tyshchenko <viktor.tyshchenko@3d4medical.com>
2017-06-04 11:16:24 +0300

    Fix proxy GET parameters.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import requests
+
+PROXY_SITE = 'https://habrahabr.ru'
 
 app = Flask(__name__.split('.')[0])
 
 
 @app.route('/<path:url>')
 def proxy(url):
-    r = requests.get('https://habrahabr.ru/{}'.format(url))
+    r = requests.get('{site}{url}'.format(site=PROXY_SITE, url=request.full_path))
     content = r.content
     return Response(content)
 
